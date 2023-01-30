1f499c595bdcc14423ff5ece1ed3879e03b139bb
Aryan Singh <58566951+aryandoescode@users.noreply.github.com>
2023-01-30 16:32:55 +0530

    Add files via upload

--- alerts.tf
+++ alerts.tf
@@ -8,13 +8,33 @@ resource "azurerm_monitor_action_group" "ag1" {
 resource "azurerm_monitor_metric_alert" "alert1" {
   name                = "alert1"
   resource_group_name = var.resource_group_name
-  scopes              = ["/subscriptions/cdc3c0fa-a22b-420a-ac28-6c56ec56eb7a/resourceGroups/rg/providers/Microsoft.Compute/virtualMachines/vm"]
+  scopes              = ["/subscriptions/cdc3c0fa-a22b-420a-ac28-6c56ec56eb7a/resourceGroups/rg1/providers/Microsoft.Compute/virtualMachines/vm1"]
   description         = "description"
   target_resource_type = "Microsoft.Compute/virtualMachines"
   
   criteria { 
     metric_namespace = "Microsoft.Compute/virtualMachines"
     metric_name      = "Percentage CPU"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = var.threshold
+
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ag1.id
+  }
+}
+resource "azurerm_monitor_metric_alert" "alert2" {
+  name                = "alert2"
+  resource_group_name = var.resource_group_name
+  scopes              = ["/subscriptions/cdc3c0fa-a22b-420a-ac28-6c56ec56eb7a/resourceGroups/rg1/providers/Microsoft.Compute/virtualMachines/vm1"]
+  description         = "description"
+  target_resource_type = "Microsoft.Compute/virtualMachines"
+  
+  criteria { 
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Memory Usage"
     aggregation      = "Total"
     operator         = "GreaterThan"
     threshold        = var.threshold
--- variable.tf
+++ variable.tf
@@ -1,15 +1,20 @@
 variable "resource_group_name" {
+    type = string
     default = "rg1"
  }
  variable "subscription_id" {
+  type = string
    default = "cdc3c0fa-a22b-420a-ac28-6c56ec56eb7a"
  }
  variable "tenant_id" {
+  type = string
    default = "2848720d-5e3d-440c-8ed8-444178758db2"
  }
  variable "location" {
+  type = string
    default = "Central India"
  }
  variable "threshold" {
+  type = number
    default = "10"
  }
